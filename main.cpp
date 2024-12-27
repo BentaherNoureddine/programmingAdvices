ba1ea534302174041baa7f5686784eb4474ff001
noureddine <noureddinebentaher20@gmail.com>
2024-12-27 21:34:47 +0100

    course 3, lesson 18 , homework 2

--- main.cpp
+++ main.cpp
@@ -5,50 +5,17 @@ using namespace std;
 int main()
 {
 
-    string Name  ;
-    int Age ;
-    string city;
-    string country ;
-    float monthly_salary;
-    float yearly_salary;
-    char gender = 'M';
-    bool isMaried;
 
-   for (int i =0; i < 10; i++)
-   {
-       cout<<"****";
-   }
-    cout<<endl;
+    int var1,var2,var3;
 
-    cout<<"Enter Your Name: ";
-    cin>>Name;
-    cout<<"Enter Your Age: ";
-    cin>>Age;
-    cout<< "Enter Your City : ";
-    cin>>city;
-    cout<< "Enter Your Country : ";
-    cin>>country;
-    cout<< "Enter Your Monthly Salary : ";
-    cin>>monthly_salary;
-    cout<< "Enter Your Yearly Salary : ";
-    cin>>yearly_salary;
-    cout<< "Enter your Gender : ";
-    cin>>gender;
-    cout<<"Aru You Maried 0/1 ";
-    cin>>isMaried;
 
-    cout<< "Name : " << Name <<endl;
-    cout<< "Age : " << Age << endl;
-    cout<< "City : "<<city<<endl;
-    cout<< "Country : "<<country<<endl;
-    cout<< "monthly_salary : "<<monthly_salary<<endl;
-    cout<< "yearly_salary : "<<yearly_salary<<endl;
-    cout<< "Gender : "<<gender<<endl;
+    cout<< "Please enter an integer Number 1 : ";
+    cin >> var1;
+    cout<< "Please enter an integer Number 2 : ";
+    cin >> var2;
+    cout<< "Please enter an integer Number 3 : ";
+    cin >> var3;
 
-    for (int i =0; i < 10; i++)
-    {
-        cout<<"*";
-    }
-
+    cout<<"Total=" <<var1+var2+var3<<endl;
     return 0;
 }
