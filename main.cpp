fbb0df50b58f19194e3a2cdf2915d9b6cf030f0c
noureddine <noureddinebentaher20@gmail.com>
2024-12-27 21:39:23 +0100

    course 3, lesson 18 , homework 3

--- main.cpp
+++ main.cpp
@@ -6,16 +6,8 @@ int main()
 {
 
 
-    int var1,var2,var3;
-
-
-    cout<< "Please enter an integer Number 1 : ";
-    cin >> var1;
-    cout<< "Please enter an integer Number 2 : ";
-    cin >> var2;
-    cout<< "Please enter an integer Number 3 : ";
-    cin >> var3;
-
-    cout<<"Total=" <<var1+var2+var3<<endl;
-    return 0;
+    int var1;
+    cout<<"Enter your age : ";
+    cin>>var1;
+    cout<<"After 5 years your age will be "<<var1+5<<" Years old.";
 }
