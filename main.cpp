eda43bf865b4c4fb43abd522642ef93865538680
noureddine <noureddinebentaher20@gmail.com>
2024-12-29 21:10:00 +0100

    course 3, lesson 34 , homework1

--- main.cpp
+++ main.cpp
@@ -3,25 +3,21 @@ using namespace std;
 
 
 
+void mySum()
+{
+   cout <<"Please enter Number1" <<endl;
+   int number1;
+   cin >> number1;
+   cout <<"Please enter Number2" <<endl;
+   int number2;
+   cin >> number2;
+   cout<<"********************************************************************" <<endl;
+   cout <<number1 +number2 <<endl;
+}
+
+
 int main()
 {
 
-   string string1;
-   string string2;
-   string string3;
-
-   cout << "Please enter String1" << endl;
-   getline(cin, string1);
-   cout << "Please enter String2" << endl;
-   getline(cin, string2);
-   cout << "Please enter String3" << endl;
-   getline(cin, string3);
-
-   cout <<"**************************************************************"<<endl;
-
-   cout << "the length of string 1 is "<<string1.length()<<endl;
-   cout <<"characters at 0,2,4,7 are: "<<string1[0]<<" "<<string1[2]<<" "<<string1[4]<<" "<<string1[7]<<endl;
-   cout <<"concatenating string2 and string 3  = "<<string2 + string3<<endl;
-   cout<< stoi(string2) <<" * "<<stoi(string3) <<" = "<<stoi(string2) * stoi(string3)<< endl;
-
+   mySum();
 }
