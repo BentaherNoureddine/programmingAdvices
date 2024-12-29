baa8640cfbf98062baeda4259a08ff23af7daf23
noureddine <noureddinebentaher20@gmail.com>
2024-12-29 22:01:07 +0100

    course 3, lesson 38 , homework1

--- main.cpp
+++ main.cpp
@@ -3,26 +3,43 @@ using namespace std;
 
 
 
-void swapme(int &a,int &b)
+struct stPerson
 {
-   int c = a;
-   a=b;
-   b=c;
+    string firstName;
+    string lastName;
+    short age;
+    int phoneNumber;
+};
 
+
+void readInfo(stPerson &person)
+{
+    cout << "Enter first name: ";
+    cin >> person.firstName;
+    cout << "Enter last name: ";
+    cin >> person.lastName;
+    cout << "Enter age: ";
+    cin >> person.age;
+    cout << "Enter phone number: ";
+    cin >> person.phoneNumber;
+}
+
+void printInfo(const stPerson& person)
+{
+    cout << "First name: " << person.firstName << endl;
+    cout << "Last name: " << person.lastName << endl;
+    cout << "Age: " << person.age << endl;
+    cout << "PhoneNumber: " << person.phoneNumber << endl;
 }
 
 
 int main()
 {
 
+    stPerson person;
+    readInfo(person);
+    printInfo(person);
 
-   int a,b;
 
-    cout<<"Enter a number a :";
-    cin>>a;
-    cout<<"Enter a number b :";
-    cin>>b;
 
-    swapme(a,b);
-    cout<<a<<" "<<b<<endl;
 }
