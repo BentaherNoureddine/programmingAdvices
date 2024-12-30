a63afc8d714de480375b4c8e2795597072ddc5ac
noureddine <noureddinebentaher20@gmail.com>
2024-12-30 20:29:28 +0100

    course 3, lesson 39 , homework1

--- main.cpp
+++ main.cpp
@@ -3,43 +3,21 @@ using namespace std;
 
 
 
-struct stPerson
-{
-    string firstName;
-    string lastName;
-    short age;
-    int phoneNumber;
-};
 
-
-void readInfo(stPerson &person)
-{
-    cout << "Enter first name: ";
-    cin >> person.firstName;
-    cout << "Enter last name: ";
-    cin >> person.lastName;
-    cout << "Enter age: ";
-    cin >> person.age;
-    cout << "Enter phone number: ";
-    cin >> person.phoneNumber;
-}
-
-void printInfo(const stPerson& person)
-{
-    cout << "First name: " << person.firstName << endl;
-    cout << "Last name: " << person.lastName << endl;
-    cout << "Age: " << person.age << endl;
-    cout << "PhoneNumber: " << person.phoneNumber << endl;
-}
 
 
 int main()
 {
 
-    stPerson person;
-    readInfo(person);
-    printInfo(person);
-
+   int x[3];
+   cout<< "Please enter Grade1 " << endl;
+   cin>>x[0];
+   cout<< "Please enter Grade2 " << endl;
+   cin>>x[1];
+   cout<< "Please enter Grade3 " << endl;
+   cin>>x[2];
+   cout <<"*****************************************************************************"<<endl;
+   cout <<" The average of grades is "<< (x[0]+x[1]+x[2])/3<<endl;
 
 
 }
