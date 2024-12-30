286d4631b7dd7386e76c6603676fbcfd9679ef20
noureddine <noureddinebentaher20@gmail.com>
2024-12-30 20:48:57 +0100

    course 3, lesson 40 , homework1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 
 
+//READ THE ARRAY
+void readArray(int x[3])
+{
+   cout<< "Please enter Grade1 " << endl;
+   cin>>x[0];
+   cout<< "Please enter Grade2 " << endl;
+   cin>>x[1];
+   cout<< "Please enter Grade3 " << endl;
+   cin>>x[2];
+}
+
+
+
+//PRINT THE ARRAY
+int getAverage(const int x[3])
+{
+   return (x[0] + x[1] + x[2]) / 3;
+
+}
 
 
 
@@ -10,14 +29,8 @@ int main()
 {
 
    int x[3];
-   cout<< "Please enter Grade1 " << endl;
-   cin>>x[0];
-   cout<< "Please enter Grade2 " << endl;
-   cin>>x[1];
-   cout<< "Please enter Grade3 " << endl;
-   cin>>x[2];
-   cout <<"*****************************************************************************"<<endl;
-   cout <<" The average of grades is "<< (x[0]+x[1]+x[2])/3<<endl;
 
+  readArray(x);
+  cout<<"thee Average of grades is "<< getAverage(x);
 
 }
