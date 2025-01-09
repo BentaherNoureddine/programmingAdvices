bbb119d89891873ffd65fd93f1593a958ea3d9b8
noureddine <noureddinebentaher20@gmail.com>
2025-01-09 22:43:16 +0100

    course 4, homework5

--- main.cpp
+++ main.cpp
@@ -2,26 +2,51 @@
 using namespace std;
 
 
-
-string readName()
+struct stInfo
 {
-    string name;
-    cout << "Enter your name: ";
-    getline(cin,name);
-    return name;
+    int age;
+    bool haveDrivingLicence;
+    bool hasRecommendation;
+};
+
+
+bool isAccepted(stInfo info)
+{
+    return (info.haveDrivingLicence && info.age>=21)||info.hasRecommendation;
 }
 
-
-void displayName(const string& name)
+void printResult(stInfo info)
 {
-    cout << "your name is " <<name<< endl;
+    if (isAccepted(info))
+    {
+        cout<<"Your account has been accepted"<<endl;
+    }else
+    {
+        cout<<"Your account has been rejected"<<endl;
+    }
+
+}
+
+stInfo readInfo()
+{
+    stInfo info;
+    cout << "Please enter your age: ";
+    cin >> info.age;
+    cout << "do you have a driving licence?: ";
+    cin >> info.haveDrivingLicence;
+    cout << "do you have a recommendation?: ";
+    cin >> info.hasRecommendation;
+    return info;
 }
 
 
 int main()
 {
 
-    displayName(readName());
+
+    printResult(readInfo());
+    return 0;
+
 
 
 }
