82599d087064e9ee873889495acbdfe11c0ec2be
noureddine <noureddinebentaher20@gmail.com>
2025-01-09 16:10:43 +0100

    course 3, lesson 49 , homework1

--- main.cpp
+++ main.cpp
@@ -2,67 +2,20 @@
 using namespace std;
 
 
-enum weekday { Sun = 1, Mon, Tue, Wed, Thu, Fri, Sat };
-
-void showWeekDays()
-{
-    cout <<"*************************************" <<endl;
-    cout << "Weekdays: \n";
-    cout<<  "(1) sunday" <<endl;
-    cout << "(2) monday" <<endl;
-    cout << "(3) tuesday" <<endl;
-    cout << "(4) wednesday" <<endl;
-    cout << "(5) thursday" <<endl;
-    cout << "(6) friday" <<endl;
-    cout << "(7) saturday" <<endl;
-    cout <<"*************************************\n\n" <<endl;
-}
-
-
-// CAST AN INTEGER TO A WEEKDAY
-weekday readWeekDay()
-{
-    //READ THE WEEKDAY
-    int day;
-    cin>>day;
-
-    // RETURN THE CASTED WEEK DAY
-    return (weekday)day;
-}
-
-// DETERMINE THE WEEK DAY BASED ON THE GIVEN INPUT
-
-string getWeekDay(weekday weekday)
-{
-    switch (weekday)
-    {
-        case Sun:
-            return "Sunday";
-        case Mon:
-            return "Monday";
-        case Tue:
-            return "Tuesday";
-        case Wed:
-            return "Wednesday";
-        case Thu:
-            return "Thursday";
-        case Fri:
-            return "Friday";
-        case Sat:
-            return "Saturday";
-        default:
-            return "Unknown";
-    }
-}
 
 
 int main()
 {
 
-    showWeekDays();
-    cout << "today is "<<getWeekDay(readWeekDay());
-    return 0;
+   for (int i=65;i<91;i++)
+   {
 
+      for (int j=65;j<91;j++)
+      {
+         cout<< static_cast<char>(i);
+         cout<<static_cast<char>(j)<<endl;
+      }
+   }
 
 
 }
