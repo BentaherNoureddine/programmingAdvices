6b53dd511ae87164b049047ba9a9434836b11a0e
noureddine <noureddinebentaher20@gmail.com>
2025-01-09 16:15:31 +0100

    course 3, lesson 49 , homework2

--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@ using namespace std;
 int main()
 {
 
-   for (int i=65;i<91;i++)
+   for (int i=10;i>=0;i--)
    {
-
-      for (int j=65;j<91;j++)
+      for (int j=1;j<=i;j++)
       {
-         cout<< static_cast<char>(i);
-         cout<<static_cast<char>(j)<<endl;
+         cout<<"*";
       }
+      cout<<endl;
    }
+
 
 
 }
