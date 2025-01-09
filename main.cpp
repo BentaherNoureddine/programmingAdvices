74c668fb48eb709b835724fe60c86b16ef5dc079
noureddine <noureddinebentaher20@gmail.com>
2025-01-09 22:27:34 +0100

    course 4, homework2

--- main.cpp
+++ main.cpp
@@ -3,19 +3,25 @@ using namespace std;
 
 
 
+string readName()
+{
+    string name;
+    cout << "Enter your name: ";
+    getline(cin,name);
+    return name;
+}
+
+
+void displayName(const string& name)
+{
+    cout << "your name is " <<name<< endl;
+}
+
 
 int main()
 {
 
-   for (int i=1; i < 11; i++)
-   {
-    for (int j=i; j < 11; j++)
-    {
-        cout<<j<<" ";
-    }
-       cout<<endl;
-   }
-
+    displayName(readName());
 
 
 }
