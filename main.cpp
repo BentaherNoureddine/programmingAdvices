6d6381b7fdd383a5e28266d8740606a767cc724c
noureddine <noureddinebentaher20@gmail.com>
2025-01-10 16:25:12 +0100

    course 4, homework11

--- main.cpp
+++ main.cpp
@@ -19,21 +19,38 @@ int sumOfNumbers(int num1,int num2,int num3)
     return num1+num2+num3;
 }
 
+
+
 float average(int num1,int num2,int num3)
 {
-    return (float) sumOfNumbers(num1,num2,num3) / 3;
+    return (float)sumOfNumbers(num1,num2,num3) / 3;
 }
 
 
 
+bool checkAverage(float average)
+{
+    return average >=50;
+}
 
+
+void prinResult(float average)
+{
+    if (checkAverage(average))
+    {
+        cout<<"Pass"<<endl;
+    }else
+    {
+        cout<<"Fail"<<endl;
+    }
+}
 
 int main()
 {
 
     int num1,num2,num3;
     readNums(num1,num2,num3);
-    cout<<"The average is: "<<average(num1,num2,num3);
+    prinResult(average(num1,num2,num3));
     return 0;
 
 
