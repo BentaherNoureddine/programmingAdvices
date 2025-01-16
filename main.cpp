67ee203f726c25b3925a598428a57bc4a5b04538
noureddine <noureddinebentaher20@gmail.com>
2025-01-16 16:22:00 +0100

    course 5, problem20/2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 
+enum    enCharType{smallLetter=1,capitalLetter=2,specialCharacter=3,digit=4};
+
 
 int randomNumber(int from,int to)
 {
@@ -12,11 +14,45 @@ int randomNumber(int from,int to)
 
 
 
+char getRandomCharacter(enCharType CharType)
+{
+    switch (CharType)
+    {
+    case enCharType::smallLetter:
+        {
+            return char(randomNumber(97, 122));
+            break;
+        }
+    case enCharType::capitalLetter:
+        {
+            return char(randomNumber(65, 90));
+            break;
+        }
+    case enCharType::specialCharacter:
+        {
+            return char(randomNumber(33, 47));
+            break;
+        }
+    case enCharType::digit:
+        {
+            return char(randomNumber(48, 57));
+            break;
+        }
+    }
+}
+
+
+
 int main()
 {
     // seeds the random number generator in c++, called only once
     srand((unsigned)time(NULL));
-    cout<<randomNumber(0,2)<<endl;
+
+
+    cout << getRandomCharacter(enCharType::smallLetter)<<endl;
+    cout<<  getRandomCharacter(enCharType::capitalLetter)<<endl;
+    cout<<  getRandomCharacter(enCharType::specialCharacter)<<endl;
+    cout<<  getRandomCharacter(enCharType::digit)<<endl;
 
 
     return 0;
