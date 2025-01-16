d247758afc5089bd9bd835e5bd6874020e084c74
noureddine <noureddinebentaher20@gmail.com>
2025-01-16 16:05:43 +0100

    course 5, problem19/2

--- main.cpp
+++ main.cpp
@@ -1,60 +1,24 @@
-#include <cmath>
+#include <cstdlib>
 #include <iostream>
 using namespace std;
 
 
 
-string readString(string s)
-{
-        string input;
-        cout<<s<<endl;
-        cin>>input;
-
-
-    return input;
-
-}
-
-
-string encrypt(string s,short encryptionKey)
-{
-    string encrypted="";
-    for (int i=0;i<s.length();i++)
-    {
-        encrypted+=char(int(s[i])+encryptionKey);
-    }
-    return encrypted;
-
-}
-
-
-string decrypt(string s,short encryptionKey)
+int randomNumber(int from,int to)
 {
 
-    string decrypted="";
-    for (int i=0;i<s.length();i++)
-    {
-        decrypted+=char(int(s[i])-encryptionKey);
-    }
-    return decrypted;
-
+    return rand()%(to-from+1)+from;
 }
 
-
-
-void printResult(string input,short encryptionKey)
-{
-    cout<<"Text Before Encryption: "<<input<<endl;
-    cout<<"Text After Encryption: "<<encrypt(input,encryptionKey)<<endl;
-    cout<<"Text After Decryption: "<<decrypt(encrypt(input,encryptionKey),encryptionKey)<<endl;
-}
 
 
 int main()
 {
-    short encryptionKey=2;
-    string input = readString("write a text");
-    printResult(input,encryptionKey);
+    // seeds the random number generator in c++, called only once
+    srand((unsigned)time(NULL));
+    cout<<randomNumber(0,2)<<endl;
+
+
     return 0;
 
 
