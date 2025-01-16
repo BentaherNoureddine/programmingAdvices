e5eb8041eaca26aaf875bc30aeaa92a86079f457
noureddine <noureddinebentaher20@gmail.com>
2025-01-16 11:01:12 +0100

    course 5, homework14/2 hadhoud solution

--- main.cpp
+++ main.cpp
@@ -24,15 +24,13 @@ int readPositiveNumber(string s)
 
 void printNumber(int number)
 {
-    int number2 = number-1;
     for (int i = number+65-1;i>=65; i--)
     {
 
-        for (int j=number2;j>=0;j--)
+        for (int j=1;j<=number-(65+number-1-i);j++)
         {
             cout<<char(i);
         }
-        number2--;
         cout<<endl;
     }
 
