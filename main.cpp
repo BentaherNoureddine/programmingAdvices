7c4ad13086dca0ef63f6fdc629c2d5ea70cc797c
noureddine <noureddinebentaher20@gmail.com>
2025-01-17 21:12:46 +0100

    course 5, problem22/2

--- main.cpp
+++ main.cpp
@@ -20,53 +20,54 @@ int readNumber(string message)
 }
 
 
-int randomNumber(int from,int to)
+void fillArray(short arr[],short n)
 {
-
-    return rand()%(to-from+1)+from;
-}
-
-
-
-string getRandomString()
-{
-    string randomString="";
-    for(int i=0;i<4;i++)
+    for (int i = 1; i <= n; i++)
     {
-     randomString+=char(randomNumber(65,90));
+        arr[i] = readNumber("Element ["+to_string(i)+"] :");
+        cout<<endl;
     }
-    return randomString;
-
 }
 
 
-void printKey(int number)
+void printArray(short arr[])
 {
-
-    string key="";
-    for(int i=1;i<=number;i++)
+    cout<<"Original Array:";
+    for (int i = 1; i <=sizeof(arr); i++)
     {
-        key=getRandomString();
-        for (int j=0;j<3;j++)
-        {
-            key+="-"+getRandomString();
-        }
-        cout<<"Key ["<<i<<"] : "<<key<<endl;
-        key="";
+        cout<<arr[i]<<" ";
     }
-
+    cout<<endl;
 }
 
+short getReputation(short arr[],short number)
+{
+    int sum =0;
+    for (int i = 0; i <sizeof(arr); i++)
+    {
+        if (arr[i] == number)
+         sum++;
+    }
+    return sum;
+}
 
+void checkNumber(short number)
+{
+    short arr[number];
+    fillArray(arr,number);
+    short numberToCheck= readNumber("Enter Number to check : ");
+    printArray(arr);
+    cout<<numberToCheck<<" is repeated "<<getReputation(arr,numberToCheck)<<" time(s)"<<endl;
+
+
+
+}
 
 
 int main()
 {
-    // seeds the random number generator in c++, called only once
-    srand((unsigned)time(NULL));
 
-
-    printKey(readNumber("Enter the number of keys you want to generate: "));
+    checkNumber(readNumber("Enter the number of elements : "));
 
 
     return 0;
