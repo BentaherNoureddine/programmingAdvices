c41962bc7c76718d3014ae9b0dd1b4e25093b838
noureddine <noureddinebentaher20@gmail.com>
2025-03-02 15:34:04 +0100

    course 5 problem 46 my solution (abs)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <cstdlib>
 #include <iostream>
 using namespace std;
 
@@ -9,67 +8,35 @@ int readNumber(string message)
 {
     short number;
 
-    do
-    {
         cout << message;
         cin >> number;
-    }while (number < 0);
+
     return number;
 
 
 }
 
 
-
-int RandomNumber(int From, int To)
-{
-
-    int randNum = rand() % (To - From + 1) + From;
-    return randNum;
-}
-
-
-void printArray(short arr[],short arrLength)
-{
-    for (int i = 0; i <arrLength; i++)
-    {
-        cout<<arr[i]<<" ";
+int getAbs(int number) {
+    if (number < 0) {
+        return -number;
     }
-    cout<<endl;
-}
-
-void fillArray(short arr[100],short arrLength)
-{
-    for (int i = 0; i < arrLength; i++)
-    {
-        arr[i] = RandomNumber(-100,100);
-    }
+    return number;
 }
 
 
-int countNegativeNumbers(short arr[100],short arrLength) {
 
-    int count = 0;
-    for (int i = 0; i < arrLength; i++) {
-        if (arr[i] < 0) {
-            count++;
-        }
-    }
-    return count;
-}
+
 
 
 int main()
 {
 
-    //Seeds the random number generator in C++, called only once
-    srand((unsigned)time(NULL));
-    short arr[100];
-    short arrayLength=readNumber("Enter the Array size : ");
-    fillArray(arr,arrayLength);
-    cout<<"Array Elements : "<<endl;
-    printArray(arr,arrayLength);
-    cout<<"Positive Numbers : "<<countNegativeNumbers(arr,arrayLength)<<endl;
+
+
+    int number = readNumber("Enter a number: ");
+    cout <<"My abs Result is: " << getAbs(number) << endl;
+    cout<<"C++ abs Result is "<< abs(number) << endl;
 
 
 
