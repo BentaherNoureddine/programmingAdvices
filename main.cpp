b8e51602d05ae1b82ada77e3719869e48a6ffe80
noureddine <noureddinebentaher20@gmail.com>
2025-07-16 22:31:43 +0100

    course 6 problem 2 : row sum of a matrix

--- main.cpp
+++ main.cpp
@@ -25,6 +25,20 @@ void printMat(int mat[3][3]) {
     }
 }
 
+int rowSum(int mat[3][3], int row) {
+    int sum = 0;
+    for (short i = 0; i < 3; i++) {
+        sum += mat[row][i];
+    }
+    return sum;
+}
+
+void printRowsSum(int mat[3][3]) {
+
+    for (short i=0;i<3;i++) {
+        cout << "Sum of row "<<i << " is "<< rowSum(mat,i)<<endl;
+    }
+}
 
 int main() {
 
@@ -34,6 +48,7 @@ int main() {
      int matrix[3][3];
      fillMat(matrix);
      printMat(matrix);
+     printRowsSum(matrix);
 
 
     return 0;
