b867aafb9ceb034c9c0c078135b1ec7b22677c37
noureddine <noureddinebentaher20@gmail.com>
2025-07-16 19:04:25 +0100

    course 6 Lesson #57 - Delete Record From File

--- main.cpp
+++ main.cpp
@@ -24,18 +24,51 @@ void loadDataFromFileToVector(string fileName, vector<string>& vFileContent ) {
 
 }
 
+void saveVectorToFile(string fileName, vector<string> vFileContent) {
+    fstream file;
+    file.open(fileName, ios::out);
+    if (file.is_open()) {
+        for (string& line : vFileContent) {
+            if (line != "") {
+               file  << line << endl;
+            }
+        }
+    }
+    file.close();
+}
+
+void deleteRecordFromFile(string fileName, string record) {
+    vector<string> vFileContent;
+    loadDataFromFileToVector(fileName,vFileContent);
+    for (string& line : vFileContent) {
+        if (line == record) {
+            line = "";
+        }
+    }
+    saveVectorToFile(fileName,vFileContent);
+
+}
 
 
+void printFile(string fileName) {
+    fstream file;
+    file.open(fileName, ios::in);
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            cout << line << endl;
+        }
+    }
+}
 
 int main() {
 
 
-    vector<string> vFileContent;
-    loadDataFromFileToVector("myFile.txt", vFileContent);
-    for (string &line : vFileContent) {
-        cout <<line << endl;
-    }
-
+    cout<< "File before delete" << endl;
+    printFile("myFile.txt");
+    deleteRecordFromFile("myFile.txt", "ali");
+    cout<< "File after delete" << endl;
+    printFile("myFile.txt");
 
     return 0;
 }
