d43256089b2835d8a8d446e65826c0859d02fcb1
noureddine <noureddinebentaher20@gmail.com>
2025-07-22 20:15:12 +0100

    course 6 problem 38: trim a string

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iomanip>
 #include <iostream>
+#include <vector>
+
 #include "myMathLibrary.h"
 #include "myInputLibrary.h"
 using namespace myInputLib;
@@ -10,16 +12,32 @@ using namespace std;
 
 
 
-char invertChar(char c) {
 
-    return isupper(c)? tolower(c):toupper(c);
+string trimLeft(string s) {
+
+
+    for (short i=0;i<s.length();i++) {
+        if (s[i]!=' ') {
+            return s.substr(i,s.length()-i);
+        }
+    }
+    return "";
 }
 
-void invertString(string& word) {
 
-    for (int i=0;i<word.length();i++) {
-        word[i]=invertChar(word[i]);
+string trimRight(string s) {
+    for (short i=s.length()-1;i>=0;i--) {
+        if (s[i]!=' ') {
+            return s.substr(0,i+1);
+        }
     }
+    return "";
+}
+
+void trim(string& s) {
+
+    s=trimLeft(s);
+    s=trimRight(s);
 }
 
 
@@ -28,10 +46,14 @@ int main() {
     //Seeds the random number generator in C++, called only once
     srand((unsigned)time(NULL));
 
-    string str = readString("Please enter your string");
-    invertString(str);
-    cout<< "String after conversion"<<endl;
-    cout<<str;
+
+    string s = readString("Enter a string");
+    cout <<"String ="<<s<<endl;
+    cout <<"Trim left ="<<trimLeft(s)<<endl;
+    cout <<"Trim Right ="<<trimRight(s)<<endl;
+    cout <<"Trim =";
+    trim(s);
+    cout <<s;
 
 
 
