cbc131ab5e5944b2a0626373625026c5aa48ff76
noureddine <noureddinebentaher20@gmail.com>
2025-07-26 22:13:50 +0100

    course 7 problem #4 : days,hours,minutes,seconds of a year

--- main.cpp
+++ main.cpp
@@ -3,34 +3,52 @@
 #include <iostream>
 #include <set>
 #include <vector>
+#include "read_save_print_clientData.h"
+using namespace fileClientData;
 
 #include "myMathLibrary.h"
 #include "myInputLibrary.h"
 
 using namespace myInputLib;
 
+
 using namespace myMathLib;
 using namespace std;
 
 
-
-bool isLeapYear(short year) {
-    return (year%4==0 && year%400==0)||(year%100==0);
-
-
+void printLeapYearTimes(int year) {
+    cout<<"Number of Days in Year "<<year<<" is 366"<<endl;
+    cout<<"Number of Hours in Year "<<year<<" is 8784"<<endl;
+    cout<<"Number of Minutes in Year "<<year<<" is 527040"<<endl;
+    cout<<"Number of seconds in Year "<<year<<" is 31622400"<<endl;
 
 }
 
-void leapYear(short year) {
-    isLeapYear(year)?cout<<year<<" is Leap Year"<<endl:cout<<year<<" is NOT LEAP year"<<endl;
+void printYearTimes(int year) {
+    cout<<"Number of Days in Year "<<year<<" is 365"<<endl;
+    cout<<"Number of Hours in Year "<<year<<" is 8760"<<endl;
+    cout<<"Number of Minutes in Year "<<year<<" is 525600"<<endl;
+    cout<<"Number of seconds in Year "<<year<<" is 31536000"<<endl;
+
 }
+
+void printTimesOfYear(int year) {
+    if (isLeapYear(year)) {
+        printLeapYearTimes(year);
+    }else {
+        printYearTimes(year);
+    }
+}
+
+
+
 
 
 int main() {
 
     //Seeds the random number generator in C++, called only once
     srand((unsigned)time(NULL));
-    leapYear(readShort("Enter a Year"));
+    printTimesOfYear(readInt("Please Enter a year to check"));
 
 
 
