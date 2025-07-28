eb0db6f642654c89757345b63368f3ff6779a125
noureddine <noureddinebentaher20@gmail.com>
2025-07-29 00:52:24 +0100

    course 7 problem #54: number of vocation days

--- main.cpp
+++ main.cpp
@@ -19,93 +19,37 @@ using namespace myMathLib;
 using namespace std;
 
 
+short numberOfVacationDays(stDate startVoc,stDate endVoc) {
 
-stDate getToday() {
-    time_t t=time(0);
-    tm* now=localtime(&t);
-
-    stDate currentDate;
-    currentDate.day=   now->tm_mday;
-    currentDate.month=now->tm_mon+1;
-    currentDate.year=now->tm_year+1900;
-    return currentDate;
-}
-
-short getDayNumber(stDate date) {
-    short a = (14 - date.month) / 12;
-    int y = date.year - a;
-    int m = date.month + 12 * a - 2;
-
-    return (date.day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
-}
-
-bool isEndOfWeek(stDate date) {
-    return getDayNumber(date)==7;
-}
-
-bool isWeekEnd(stDate date) {
-    return getDayNumber(date)==6;
-}
-
-bool isBusinessDay(stDate date) {
-    return !(isEndOfWeek(date)&&isWeekEnd(date));
-}
-
-short daysUntilTheEndOfWeek(stDate date) {
     short days=0;
-    for (short i=getDayNumber(date);i<7;i++) {
-        days++;
-    }
-    return days;
-}
-
-short daysUntilTheEndOfMonth(stDate date) {
-    return getNumberOfDaysInMonth(date.year,date.month)-date.day;
-}
-
-short daysUntilTheEndOfYear(stDate date) {
-
-    stDate endOfYear;
-    endOfYear.year=date.year;
-    endOfYear.month=12;
-    endOfYear.day=31;
-    short days=0;
-    while (compareDates(date,endOfYear)) {
-        days++;
-        increaseDateByXDays(date,1);
+    while (compareDates(startVoc,endVoc)) {
+        if (isBusinessDay(startVoc)) {
+            days++;
+            startVoc=increaseDateByXDays(startVoc,1);
+        }else {
+            startVoc=increaseDateByXDays(startVoc,1);
+        }
     }
     return days;
 
-
 }
 
-string getTodayString(stDate date) {
-    string days[]={"Sun","Mon","Tue","Wed","Thir","Fri","Satur",};
 
-    return "Today is "+days[getDayNumber(date)]+" , "+printDate(date);
-}
+
 
 int main() {
 
     //Seeds the random number generator in C++, called only once
     srand((unsigned)time(NULL));
-    stDate date=getToday();
-
-    cout<<getTodayString(date);
-
-    cout<<"is it end of week?"<<endl;
-    isEndOfWeek(date)?cout<<"Yes it's end of week."<<endl:cout<<"No it s NOT end of week."<<endl;
-
-    cout<<"is it weekend?"<<endl;
-    isWeekEnd(date)?cout<<"Yes it's weekend."<<endl:cout<<"No it s NOT weekend."<<endl;
+    cout<<"Vacation Starts :"<<endl;
+    stDate startVac=readDate();
+    cout<<"Vacation Ends : "<<endl;
+    stDate endVac=readDate();
+    cout<<"Vacation from : "<<getDayString(startVac)<<endl;
+    cout<<"Vacation To : "<<getDayString(endVac)<<endl;
+    cout<<"\n\n\n Actual Vacation days is : "<<numberOfVacationDays(startVac,endVac)<<endl;
 
 
-    cout<<"is it Business Day?"<<endl;
-    isBusinessDay(date)?cout<<"Yes it's Business day."<<endl:cout<<"No it s NOT a business day."<<endl;
-
-    cout<<"Days until end of week "<<daysUntilTheEndOfWeek(date)<<" Day(s)"<<endl;
-    cout<<"Days until end of month "<<daysUntilTheEndOfMonth(date)<<" Day(s)"<<endl;
-    cout<<"Days until end of year "<<daysUntilTheEndOfYear(date)<<" Day(s)"<<endl;
 
 
 
--- myLibrary.h
+++ myLibrary.h
@@ -543,6 +543,72 @@ void printAllDecreases(stDate date) {
 
 
 
+    stDate getToday() {
+        time_t t=time(0);
+        tm* now=localtime(&t);
+
+        stDate currentDate;
+        currentDate.day=   now->tm_mday;
+        currentDate.month=now->tm_mon+1;
+        currentDate.year=now->tm_year+1900;
+        return currentDate;
+    }
+
+    short getDayNumber(stDate date) {
+        short a = (14 - date.month) / 12;
+        int y = date.year - a;
+        int m = date.month + 12 * a - 2;
+
+        return (date.day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
+    }
+
+    bool isEndOfWeek(stDate date) {
+        return getDayNumber(date)==6;
+    }
+
+    bool isWeekEnd(stDate date) {
+        return getDayNumber(date)==5;
+    }
+
+    bool isBusinessDay(stDate date) {
+        return !(isEndOfWeek(date)||isWeekEnd(date));
+    }
+
+    short daysUntilTheEndOfWeek(stDate date) {
+        short days=0;
+        for (short i=getDayNumber(date);i<7;i++) {
+            days++;
+        }
+        return days;
+    }
+
+    short daysUntilTheEndOfMonth(stDate date) {
+        return getNumberOfDaysInMonth(date.year,date.month)-date.day;
+    }
+
+    short daysUntilTheEndOfYear(stDate date) {
+
+        stDate endOfYear;
+        endOfYear.year=date.year;
+        endOfYear.month=12;
+        endOfYear.day=31;
+        short days=0;
+        while (compareDates(date,endOfYear)) {
+            days++;
+            increaseDateByXDays(date,1);
+        }
+        return days;
+
+
+    }
+
+    string getDayString(stDate date) {
+        string days[]={"Sun","Mon","Tue","Wed","Thir","Fri","Satur",};
+
+        return "Today is "+days[getDayNumber(date)]+" , "+printDate(date);
+    }
+
+
 
 
 
