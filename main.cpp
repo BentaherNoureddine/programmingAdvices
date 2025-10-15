f68b503332cbe7d1c179b50c9d98ee9495a44b84
noureddine <noureddinebentaher20@gmail.com>
2025-10-15 11:37:52 +0100

    course 12 doubly Linked List :
    insert after

--- main.cpp
+++ main.cpp
@@ -7,18 +7,25 @@ class Node {
 public:
     T data;
     Node *next ;
-
+    Node *prev;
 };
 
 
 template <typename T>
 void addAtTheBeginning(Node<T> * &head,T data) {
 
+    if (head==nullptr) {
+        return;
+    }
+
     Node<T> * newNode=new Node<T>;
 
     newNode->data=data;
 
     newNode->next=head;
+    newNode->prev=nullptr;
+
+    head->prev=newNode;
 
     head=newNode;
 }
@@ -34,7 +41,7 @@ void printList(Node<T> * head) {
 }
 
 
-template<typename T>
+template <typename T>
 Node<T> *findNode(Node<T> *head,T data) {
     while (head!=nullptr) {
         if (head->data==data) {
@@ -45,120 +52,47 @@ Node<T> *findNode(Node<T> *head,T data) {
     return nullptr;
 }
 
+template <typename T>
+void insertAfter(Node<T> *head,T after,T data) {
+    if (head==nullptr) {
+        return;
+    }
 
-template<typename T>
-void insertAfter(Node<T> *position,T data) {
-       Node<T> *newNode = new Node<T>;
-       newNode->next=position->next;
-       newNode->data=data;
-       position->next=newNode;
-}
-
-template<typename T>
-void insertAtEnd(Node<T> *&head,T data) {
-
-    Node<T> *newNode =new Node<T>;
+    Node<T> *newNode= new Node<T>;
     newNode->data=data;
-    newNode->next=nullptr;
+
+    Node<T> *node=findNode<short>(head,after);
+
+    Node<T> *nextNode=node->next;
 
 
-    if (head==nullptr) {
-        head = new Node<T>();
-        head=newNode;
-        return;
-    }
-
-    Node<short> *node=head;
-    while (node->next!=nullptr) {
-        node=node->next;
-    }
     node->next=newNode;
+
+    newNode->prev=node;
+    newNode->next=nextNode;
+    nextNode->prev=newNode;
+
+
+
 }
-
-
-template<typename T>
-void deleteNode(Node<T>* &head,T data) {
-    if (head==nullptr) {
-        return;
-    }
-    if (head->next==nullptr) {
-        delete head;
-        return;
-    }
-    Node<T> *node =findNode<T>(head,data);
-    if (node!=nullptr) {
-        if (node==head) {
-            head=head->next;
-            delete node;
-            return;
-        }
-        Node<T> *tempNode=head;
-        while (true) {
-
-            if (tempNode->next==node) {
-                if (node->next==nullptr) {
-                    tempNode->next=nullptr;
-                    break;
-                }
-                tempNode->next=node->next;
-                break;
-            }
-            tempNode=tempNode->next;
-        }
-    }
-}
-
-template<typename T>
-void deleteFirstNode(Node<T> * &head) {
-    if (head==nullptr) {
-        return;
-    }
-
-    if (head->next==nullptr) {
-        delete head;
-        return;
-    }
-
-    const Node<T> *temp=head;
-    head=head->next;
-    delete temp;
-}
-
-template<typename T>
-void deleteLastNode(Node<T> *head) {
-    Node<T> * prev=head,*current=head;
-
-    if (head==nullptr) {
-        return;
-    }
-    if (head->next==nullptr) {
-        delete head;
-    }
-    while (current->next!=nullptr) {
-        prev=current;
-        current=current->next;
-    }
-    prev->next=nullptr;
-    delete current;
-}
-
 
 
 int main()
 
 {
-    Node<short> *head = nullptr;
+    Node<short> *head = new Node<short>;
+    head->data=1;
 
 
 
-    insertAtEnd<short>(head,1);
-    insertAtEnd<short>(head,2);
-    insertAtEnd<short>(head,3);
-    insertAtEnd<short>(head,7);
+    addAtTheBeginning<short>(head,2);
+    addAtTheBeginning<short>(head,3);
     std::cout<<"before"<<std::endl;
     printList(head);
 
-    deleteLastNode<short>(head);
+    insertAfter<short>(head,2,7);
+
+
     std::cout<<"after"<<std::endl;
     printList<short>(head);
 
